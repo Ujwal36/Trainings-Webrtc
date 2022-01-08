853fa60088765202a2d0435997871169d2ab3467
Ujwal36 <35258482+Ujwal36@users.noreply.github.com>
2022-01-08 12:25:57 +0530

    Update client.js

--- client.js
+++ client.js
@@ -11,3 +11,33 @@ function errorCallback(error) {
 console.log("Error", error);
 }
 navigator.getUserMedia(constraints,successCallback, errorCallback);
+
+btnGetAudioTracks.addEventListener("click", function(){ 
+   console.log("getAudioTracks"); 
+   console.log(stream.getAudioTracks()); 
+});
+  
+btnGetTrackById.addEventListener("click", function(){ 
+   console.log("getTrackById"); 
+   console.log(stream.getTrackById(stream.getAudioTracks()[0].id)); 
+});
+  
+btnGetTracks.addEventListener("click", function(){ 
+   console.log("getTracks()"); 
+   console.log(stream.getTracks()); 
+});
+ 
+btnGetVideoTracks.addEventListener("click", function(){ 
+   console.log("getVideoTracks()"); 
+   console.log(stream.getVideoTracks()); 
+});
+
+btnRemoveAudioTrack.addEventListener("click", function(){ 
+   console.log("removeAudioTrack()"); 
+   stream.removeTrack(stream.getAudioTracks()[0]); 
+});
+  
+btnRemoveVideoTrack.addEventListener("click", function(){ 
+   console.log("removeVideoTrack()"); 
+   stream.removeTrack(stream.getVideoTracks()[0]); 
+});
